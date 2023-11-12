4ed8a3984a8bcdd04b67682d88f0545c0653eb0c
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-11-13 01:29:29 +0300

    statistics

--- SignalRWebUI/Controllers/ContactController.cs
+++ SignalRWebUI/Controllers/ContactController.cs
@@ -20,6 +20,7 @@ namespace SignalRWebUI.Controllers
 			var responseMessage = await client.GetAsync("https://localhost:7122/api/Contact");
 			if (responseMessage.IsSuccessStatusCode)
 			{
+
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
 				var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
 				return View(values);
---
+++ SignalRWebUI/Controllers/ProgressBarsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+
+	public class ProgressBarsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

9e804123b2f1fdb0d1e9b3fc0d9d1d2be83a437a
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-11-12 14:57:48 +0300

    architectures of order and order detail

---
+++ SignalR.BusinessLayer/Concrete/OrderDetailManager.cs
@@ -0,0 +1,48 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+	public class OrderDetailManager : IOrderDetailService
+	{
+		private readonly IOrderDetailDal _orderDetailDal;
+
+		public OrderDetailManager(IOrderDetailDal orderDetailDal)
+		{
+			_orderDetailDal = orderDetailDal;
+		}
+
+		public void TAdd(OrderDetail entity)
+		{
+			_orderDetailDal.Add(entity);
+		}
+
+		public void TDelete(OrderDetail entity)
+		{
+		_orderDetailDal.Delete(entity);
+		}
+
+		public OrderDetail TGetById(int id)
+		{
+			var value=_orderDetailDal.GetById(id);
+			return value;
+		}
+
+		public List<OrderDetail> TGetListAll()
+		{
+			var values=_orderDetailDal.GetListAll();
+			return values;
+		}
+
+		public void TUpdate(OrderDetail entity)
+		{
+			_orderDetailDal.Update(entity);
+		}
+	}
+}

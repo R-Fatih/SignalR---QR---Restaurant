2186d1f8fb8267c74d78008a6b67e6cb77c602e0
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-11-12 12:50:20 +0300

    signalR implementations

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -3,9 +3,24 @@ using SignalR.BusinessLayer.Concrete;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.EntityFramework;
+using SignalRApi.Hubs;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host)=>true)
+        .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
+
 
 // Add services to the container.
 builder.Services.AddDbContext<SignalRContext>();
@@ -54,10 +69,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();

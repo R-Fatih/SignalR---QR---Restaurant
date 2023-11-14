da0b450b58e1874c467a6ea78e545fa11c674563
RFatih <39864827+R-Fatih@users.noreply.github.com>
2023-11-14 12:52:45 +0300

    api consumes for main page

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDtos;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
 	public class _DefaultOurMenuComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOurMenuComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7122/api/Product/ProductListWitchCategory");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+
+        }
+    }
 }
